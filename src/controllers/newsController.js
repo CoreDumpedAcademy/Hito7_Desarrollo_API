531fa6c82fe536ad3d59a296f800cf581a4698c7
osmak <osmak@protonmail.com>
2019-04-11 21:14:02 +0200

    Added new params to Endpoint Everything: {page, pageSize, from (date), until (date), sortBy}

--- src/controllers/newsController.js
+++ src/controllers/newsController.js
@@ -2,6 +2,7 @@ const newsapi_config = require('../newsapi_token')
 const NewsAPI = require('newsapi');
 const newsapi = new NewsAPI(newsapi_config.token);
 const newsStructs = require("../middlewares/newsStructures");
+var moment = require('moment');
 
 /**
  * 
@@ -27,6 +28,14 @@ async function getSources(idioma) {
     return sourcesArray
 }
 
+function checkDates(date){
+    if(!moment(date, moment.ISO_8601).isValid() && !moment(date,"YYYY-MM-DD").isValid()){
+        return false
+    }else{
+        return true
+    }
+}
+
 /**
  * 
  * @param {*} req 
@@ -44,17 +53,50 @@ async function getNews(req, res) {
     var idioma = req.query.lang
     var busqueda = req.query.q
     var fuentes = req.query.sources
+    var pagina = req.query.page
+    var tamañoPagina = req.query.pageSize
+    var fromDate = req.query.from
+    var untilDate = req.query.until
+    var ordenar = req.query.sortBy
+
+    //si las fechas no corresponden se pone esas por defecto
+    if(!checkDates(fromDate) || fromDate===undefined){
+        fromDate = new Date(moment().format("YYYY-MM-DD"))
+        fromDate.setMonth(fromDate.getMonth()-1)
+        fromDate+=""
+    }
+
+    if(!checkDates(untilDate) || untilDate===undefined){
+        untilDate = new Date(moment().format("YYYY-MM-DD"))
+        untilDate+=""
+    }
 
     console.log(req.query)
+
+    console.log(fromDate+ " -- " +untilDate)
 
     //si no se indica idioma, se utilizará inglés
     if (!newsStructs.languagesArray.includes(idioma)) {
         idioma = 'en'
     }
 
+    if (!newsStructs.SortByArray.includes(ordenar)) {
+        ordenar = 'popularity'
+    }
+
     //si no se indica una fuente se utilizaran todas del idioma seleccionado
     if(fuentes===undefined){
         fuentes = await getSources(idioma)
+    }
+
+    //Si pagina no es un numero entero se mostraran resultados de la pagina 1
+    if (!Number.isInteger(pagina) || tamañoPagina<1){
+        pagina = 1
+    }
+
+    //Si el tamaño de la pagina no es un numero pues se utilizara 20(se mostraran 20 noticias)
+    if (!Number.isInteger(tamañoPagina) || tamañoPagina<1){
+        tamañoPagina = 20
     }
 
     console.log(fuentes)
@@ -65,9 +107,12 @@ async function getNews(req, res) {
             sources: fuentes,
             //country: pais, Parece ser que la api no lo soporta
             language: idioma,
+            from: fromDate,
+            to: untilDate,
             //category: categoria, Igual que lo otro
-            sortBy: 'relevancy',
-            page: 2
+            sortBy: ordenar,
+            page: pagina,
+            pageSize: tamañoPagina
         }).then(response => {
             res.status(200).send({ response })
         }).catch(err => {
@@ -81,8 +126,9 @@ async function getNews(req, res) {
             language: idioma,
             //category: categoria, Igual que lo otro
             q: busqueda,
-            sortBy: 'relevancy',
-            page: 2
+            sortBy: ordenar,
+            page: pagina,
+            pageSize: tamañoPagina
         }).then(response => {
             res.status(200).send({ response })
         }).catch(err => {
