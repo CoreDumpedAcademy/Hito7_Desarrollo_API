d0aea5d9d69994e99fa898fe664a98574f6af407
osmak <osmak@protonmail.com>
2019-04-11 16:16:35 +0200

    Funcion para todas las noticias

--- src/controllers/newsController.js
+++ src/controllers/newsController.js
@@ -1,29 +1,28 @@
 const newsapi_config = require('../newsapi_token')
 const NewsAPI = require('newsapi');
 const newsapi = new NewsAPI(newsapi_config.token);
+const enume = require("../middlewares/enumStructures");
 
 /**
  * @param {*} req 
  * @param {*} res 
- * Esta funcion devuelve un string con todas las fuentes que corresponden con el language y country indicado
- * TO DO: poder pasar language y country por parametros mediante sobrecarga de constructores
+ * Esta funcion devuelve un string con todas las fuentes que corresponden con el language indicado
  */
-async function getSources(req, res) {
-    console.log(req.params.lang)
+async function getSources(idioma) {
+
     var sourcesArray = ''
     await newsapi.v2.sources({
-        language: 'en',
-        country: 'us',
+        language: idioma
     }).then(response => {
         var sourcesLength = response.sources.length
 
         for (var i = 0; i < sourcesLength; i++) {
-            sourcesArray+=response.sources[i].id+", "
+            sourcesArray += response.sources[i].id + ", "
         }
-        console.log(sourcesArray)
+        console.log("s: " + sourcesArray)
     }).catch(err => {
         console.log(err)
-        res.status(500)
+        return 'fox-news'
     });
     return sourcesArray
 }
@@ -32,144 +31,68 @@ async function getSources(req, res) {
  * 
  * @param {*} req 
  * @param {*} res 
- * Esta function obtiene todas las noticias de las fuentes que recibe por el parametro sources, ver funcion getSources
- * To DO: sortBy, pasar por parametro como queremos que se ordene
- */
+ * Esta function obtiene todas las noticias 
+ * Los filtros que soporta son idioma, fuentes y query (busqueda)  
+ * idioma por defecto: en (inglés)
+ * Sources por defecto: *
+ * query (búsqueda) por defecto: *
+ * TO-DO: Añadir soporte para buscar por fechas
+ **/
 async function getNews(req, res) {
-    var param = [req.params.param1, req.params.param2, req.params.param3]
 
-    var country = req.params.country
-    var language = req.params.lang
-    var categoria = req.params.category
+    var idioma = req.query.lang
+    var pais = req.query.country
+    var categoria = req.query.categoria
+    var busqueda = req.query.q
+    var fuentes = req.query.sources
 
-    //Posibles parametros
-    var paramNewsArray = [
-        "lang", //idioma de la noticia
-        "country", //Pais de la noticia
-        "categ" //Categoria de la noticia
-    ]
-    var countriesArray = [
-        "ae", //Emiratos Árabes Unidos
-        "ar", //Argentina
-        "at", //Austria
-        "au", //Australia
-        "bg", //Bulgaria
-        "br", //Brasil
-        "ca", //Canadá
-        "ch", //Suiza
-        "cn", //China
-        "co", //Colombia
-        "cu", //Cuba
-        "cz", //República Checa
-        "de", //Alemania
-        "eg", //Egipto
-        "fr", //Francia
-        "gb", //Reino Unido
-        "gr", //Grecia
-        "hk", //Hong Kong
-        "hu", //Hungría
-        "id", //Indonesia
-        "ie", //Irlanda
-        "il", //Israel
-        "in", //India
-        "it", //Italia
-        "jp", //Japón
-        "kr", //Corea del Sur
-        "lt", //Lituania
-        "lv", //Letonia
-        "ma", //Marruecos
-        "mx", //México
-        "my", //Malasia
-        "ng", //Nigeria
-        "nl", //Paises Bajos
-        "no", //Noruega
-        "nz", //Nueva Zelanda
-        "ph", //Filipinas
-        "pl", //Polonia
-        "pt", //Portugal
-        "ro", //Rumania
-        "rs", //Serbia
-        "ru", //Rusia
-        "sa", //Arabia Saudita
-        "se", //Suecia
-        "sg", //Singapur
-        "si", //Eslovenia
-        "sk", //Eslovaquia
-        "th", //Tailandia
-        "tr", //Turquia
-        "tw", //Taiwán
-        "ua", //Ucrania
-        "us", //Estados Unidos
-        "ve", //Venezuela
-        "za" //Sudáfrica
-    ]
-    var languagesArray = [
-        "ar", //Arabe
-        "de", //Aleman
-        "en", //Inglés
-        "es", //Español
-        "fr", //Francés
-        "he", //Hebreo
-        "it", //Italiano
-        "nl", //Holandés
-        "no", //Noruego
-        "pt", //Portugués
-        "ru", //Ruso
-        "se", //Sami
-        "ud", //??????
-        "zh"  //Chino
-    ]
-    var categoriesArray = [
-        "business",
-        "entertainment",
-        "general",
-        "health",
-        "science",
-        "sports",
-        "technology",
-        "business, entertainment, general, health, science, sports, technology" //todas las categorias
-    ]
+    console.log(req.query)
 
-    for(let i=0;i<param.length;i++){
-        if(param[i]===undefined){
-            if(!param.includes("categ") && param[i]===undefined ){
-                param[i]=="categ"
-                categoria = categoriesArray[(categoriesArray.length)-1]
-            }
-            if(!param.includes("country") && param[i]===undefined ){
-                param[i]=="country"
-                country = 'us'
-            }
-            if(!param.includes("lang") && param[i]===undefined ){
-                param[i]=="lang"
-                country = 'us'
-            }
-        }
+    if (!enume.countriesArray.includes(pais)) {
+        pais = 'us'
     }
 
-    if (!countriesArray.includes(country)){
-        country = 'us'
+    if (!enume.languagesArray.includes(idioma)) {
+        idioma = 'en'
     }
 
-    if (!languagesArray.includes(language)){
-        lang = 'en'
+    if (!enume.categoriesArray.includes(categoria)) {
+        categoria = enume.categoriesArray[enume.categoriesArray.length - 1]
     }
 
-    if (!categoriesArray.includes(categoria)){
-        categoria = categoriesArray[categoriesArray.length-1]
+    if(fuentes===undefined){
+        fuentes = await getSources(idioma)
     }
 
-    console.log(country+" - "+language)
-    var fuentes = ''
-    fuentes = await getSources()
     console.log(fuentes)
-    await newsapi.v2.everything({
-        sources: fuentes,
-        sortBy: 'relevancy',
-        page: 2
-    }).then(response => {
-        res.status(200).send({ response })
-    })
+    if (busqueda === undefined) {
+        await newsapi.v2.everything({
+            sources: fuentes,
+            //country: pais, Parece ser que la api no lo soporta
+            language: idioma,
+            //category: categoria, Igual que lo otro
+            sortBy: 'relevancy',
+            page: 2
+        }).then(response => {
+            res.status(200).send({ response })
+        }).catch(err => {
+            res.status(500).send({Message:`Error: ${err}`})
+        })
+    } else {
+        await newsapi.v2.everything({
+            sources: fuentes,
+            //country: pais, Parece ser que la api no lo soporta
+            language: idioma,
+            //category: categoria, Igual que lo otro
+            q: busqueda,
+            sortBy: 'relevancy',
+            page: 2
+        }).then(response => {
+            res.status(200).send({ response })
+        }).catch(err => {
+            res.status(500).send({Message:`Error: ${err}`})
+        })
+    }
 }
 
 module.exports = {
--- src/routes/newsRoutes.js
+++ src/routes/newsRoutes.js
@@ -5,7 +5,12 @@ const api = express.Router()
 
 const newsController = require('../controllers/newsController')
 
+/** Ruta noticias
+ *  pueden usarse de query lang, sources y q
+ *  ejemplo:
+ *  http://localhost:3000/api/news?lang=en&sources=CNN&q=Bitcoin
+ *  los parametros son opcionales y se pueden enviar en cualquier orden
+ * */
 api.get('/news', newsController.getNews);
-api.get('/news/sources', newsController.getSources);
 
 module.exports = api;
